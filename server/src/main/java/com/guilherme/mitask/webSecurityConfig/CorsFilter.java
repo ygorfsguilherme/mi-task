5f44f567aabdb5a274f52126656df05cf482b933
Ygor Guilherme <ygorfsguilherme@gmail.com>
2023-08-12 01:02:51 -0300

    adjustments in cors

--- server/src/main/java/com/guilherme/mitask/webSecurityConfig/CorsFilter.java
+++ server/src/main/java/com/guilherme/mitask/webSecurityConfig/CorsFilter.java
@@ -1,16 +1,18 @@
 package com.guilherme.mitask.webSecurityConfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsFilter implements WebMvcConfigurer {
-
+    @Value("${mitask.cors.origins}")
+    String origin;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/")
+        registry.addMapping("/*/**")
+                .allowedOrigins(origin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
